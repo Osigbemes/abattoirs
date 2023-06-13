6beb6908bb8c81d1e2be86415dbcef3a76fab256
Osigbemes <osinaikegbemileke@gmail.com>
2023-06-13 14:23:20 +0100

    project set up

--- proteintech/settings/base.py
+++ proteintech/settings/base.py
@@ -51,6 +51,7 @@ LOCAL_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
