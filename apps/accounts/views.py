af7d4288f02c380153245b1d8fee7a0b4d5d177a
Osigbemes <osinaikegbemileke@gmail.com>
2023-06-13 15:58:54 +0100

    user api view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.generics import APIView
+from accounts.models import User
+from accounts.ser import UserSerializer
 
-# Create your views here.
+class GeneralClassView(APIView):
+    queryset = User
+    
