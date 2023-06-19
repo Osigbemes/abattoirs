9a095278ba3eb9c4f221a43c1ec9316634e0fbe7
Osigbemes <osinaikegbemileke@gmail.com>
2023-06-19 14:12:21 +0100

    email service

--- proteintech/settings/base.py
+++ proteintech/settings/base.py
@@ -192,12 +192,13 @@ SOCIAL_MEDIA_LINKEDIN_URL = "https://linkedin.com"
 SOCIAL_MEDIA_TWITTER_URL = "https://twitter.com"
 URL_PLACEHOLDERS = "https:/wecatech.com"
 
+EMAIL_BACKEND = config("EMAIL_BACKEND")
+EMAIL_USE_SSL = config("EMAIL_USE_SSL")
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_BACKEND = config("EMAIL_BACKEND")
-EMAIL_USE_SSL = config("EMAIL_USE_SSL")
+
 
 # EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_USE_SSL = True
