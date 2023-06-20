ca0e6f8d7a52d06e7f6c3d13313d058e322fa87b
Osigbemes <osinaikegbemileke@gmail.com>
2023-06-20 14:22:46 +0100

    certificate service

---
+++ apps/utils/certificate/certificate.py
@@ -0,0 +1,52 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+from reportlab.platypus import SimpleDocTemplate, Image, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+from io import BytesIO
+
+def generate_certificate(certificate_data):
+    doc = SimpleDocTemplate("certificate.pdf", pagesize=letter)
+
+    # Add content to the PDF
+    story = []
+    styles = getSampleStyleSheet()
+    
+    # Add image to the PDF
+    image_path = 'path_to_your_image.png'
+    image = Image(image_path, width=2.5*inch, height=2.5*inch)
+    story.append(image)
+    
+    # Add text to the PDF
+    recipient_name = certificate_data['recipient_name']
+    text = f"Certificate of Achievement\n\nThis is to certify that {recipient_name} has successfully completed the course."
+    paragraph = Paragraph(text, styles["BodyText"])
+    story.append(paragraph)
+
+    doc.build(story)
+
+    return "certificate.pdf"
+
+def generate_certificate_for_abattoir(certificate_data):
+    pdf_buffer = BytesIO()
+    doc = SimpleDocTemplate(pdf_buffer, pagesize=letter)
+
+    # Add content to the PDF
+    story = []
+    styles = getSampleStyleSheet()
+    
+    # Add image to the PDF
+    image_path = 'path_to_your_image.png'
+    image = Image(image_path, width=2.5*inch, height=2.5*inch)
+    story.append(image)
+    
+    # Add text to the PDF
+    recipient_name = certificate_data['recipient_name']
+    text = f"Certificate of Achievement\n\nThis is to certify that {recipient_name} has successfully completed the course."
+    paragraph = Paragraph(text, styles["BodyText"])
+    story.append(paragraph)
+
+
+    doc.build(story)
+
+    pdf_buffer.seek(0)
+    return pdf_buffer
--- proteintech/settings/base.py
+++ proteintech/settings/base.py
@@ -144,7 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
