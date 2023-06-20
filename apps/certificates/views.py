e13e8cf68c2ba743b621212b3ebb116233796945
Osigbemes <osinaikegbemileke@gmail.com>
2023-06-20 17:23:23 +0100

    download certificate

--- apps/certificates/views.py
+++ apps/certificates/views.py
@@ -1,9 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from apps.certificates.serializers import IssueCertificateSerializer, CertificateSerializer
 from apps.certificates.models import Certificate
 from rest_framework.exceptions import NotFound
+from rest_framework.decorators import action
+from apps.utils.services.certificate import generate_certificate, generate_certificate_for_abattoir
+from rest_framework.response import Response
 
 class IssueCertificateViewSet(viewsets.ModelViewSet):
     queryset = Certificate.objects.all()
@@ -15,6 +19,19 @@ class IssueCertificateViewSet(viewsets.ModelViewSet):
     
     def get(self, request, *args, **kwargs):
         raise NotFound()
+    
+    @action(
+    methods=["post"],
+    detail=False,
+    url_path="download_certificate",
+    permission_classes=[IsAuthenticated]
+    )
+    def get_certificate(self, request):
+        user = request.user
+        name = user.firstname
+        certificate = generate_certificate(request)
+        return HttpResponse(certificate, content_type='application/pdf')
+    
     
 class CertificateViewSet(viewsets.ModelViewSet):
     queryset = Certificate.objects.all()
--- apps/utils/services/certificate.py
+++ apps/utils/services/certificate.py
@@ -3,28 +3,54 @@ from reportlab.lib.units import inch
 from reportlab.platypus import SimpleDocTemplate, Image, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 from io import BytesIO
+import os
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
 
-def generate_certificate(certificate_data):
-    doc = SimpleDocTemplate("certificate.pdf", pagesize=letter)
+def generate_certificate(request):
+    # Create a PDF document
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="certificate.pdf"'
 
-    # Add content to the PDF
-    story = []
-    styles = getSampleStyleSheet()
-    
-    # Add image to the PDF
-    image_path = 'logos.logo.png'
-    image = Image(image_path, width=2.5*inch, height=2.5*inch)
-    story.append(image)
-    
+    # Create a canvas to draw on
+    c = canvas.Canvas(response)
+
     # Add text to the PDF
-    recipient_name = certificate_data['recipient_name']
-    text = f"Certificate of Achievement\n\nThis is to certify that {recipient_name} has successfully completed the course."
-    paragraph = Paragraph(text, styles["BodyText"])
-    story.append(paragraph)
+    c.drawString(100, 700, "Certificate of Achievement")
+    c.drawString(100, 650, "This is to certify that John Doe")
+    c.drawString(100, 600, "has successfully completed the course")
 
-    doc.build(story)
+    # Add an image to the PDF
+    c.drawInlineImage('logo.png', 100, 400, width=200, height=200)
 
-    return "certificate.pdf"
+    # Save the PDF and close the canvas
+    c.save()
+
+    return response
+
+
+# def generate_certificate(certificate_data):
+#     doc = SimpleDocTemplate("certificate.pdf", pagesize=letter)
+
+#     # Add content to the PDF
+#     story = []
+#     styles = getSampleStyleSheet()
+    
+#     # Add image to the PDF
+#     # image_path = os.path.join(static_dir, 'images', 'logo.png')
+#     image_path = 'logo.png'
+#     image = Image(image_path, width=2.5*inch, height=2.5*inch)
+#     story.append(image)
+    
+#     # Add text to the PDF
+#     recipient_name = certificate_data['recipient_name']
+#     text = f"Certificate of Achievement\n\nThis is to certify that {recipient_name} has successfully completed the course."
+#     paragraph = Paragraph(text, styles["BodyText"])
+#     story.append(paragraph)
+
+#     doc.build(story)
+
+#     return "certificate.pdf"
 
 def generate_certificate_for_abattoir(certificate_data):
     pdf_buffer = BytesIO()
@@ -35,7 +61,7 @@ def generate_certificate_for_abattoir(certificate_data):
     styles = getSampleStyleSheet()
     
     # Add image to the PDF
-    image_path = 'logos.logo.png'
+    image_path = 'logo.png'
     image = Image(image_path, width=2.5*inch, height=2.5*inch)
     story.append(image)
     
@@ -52,3 +78,11 @@ def generate_certificate_for_abattoir(certificate_data):
     return pdf_buffer
 
 generate_certificate({'recipient_name':'osigbemes'})
+
+# def find_missing_number(num, *array):
+#     if num in array:
+#         return num
+#     print (num in array)
+#     print ('number not found')
+    
+# find_missing_number(3, ([1,2,3,4,5,6]))
