6d00d6d7aeb71c5a6aeb506d08eeae3f4a58a753
Osigbemes <osinaikegbemileke@gmail.com>
2023-08-05 22:14:49 +0100

    celery tasks

--- proteintech/settings/base.py
+++ proteintech/settings/base.py
@@ -7,11 +7,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 # django-insecure-%1uu!zj-3&q1&244!rw3yt7hu%v=r*1=ku1n7-(3%nsanxydal
 SECRET_KEY = config("SECRET_KEY")
 
--- proteintech/settings/celery.py
+++ proteintech/settings/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proteintech.settings.base')
 app = Celery('proteintech')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', 'proteintech.settings.base')
 
 # Load task modules from all registered Django app configs. 
 app.autodiscover_tasks()
