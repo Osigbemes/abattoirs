8225c9008c780d6100401a9c1e12967c7cc76238
Osigbemes <osinaikegbemileke@gmail.com>
2023-08-05 22:47:29 +0100

    celery tasks

--- proteintech/settings/celery.py
+++ proteintech/settings/celery.py
@@ -8,13 +8,8 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 # REDIS related settings 
-REDIS_HOST = 'localhost' 
+REDIS_HOST = '127.0.0.1' 
 REDIS_PORT = '6379' 
 BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0' 
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600} 
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
-
-
-# Set broker_connection_retry and broker_connection_retry_on_startup
-CELERY_BROKER_CONNECTION_RETRY = True
-CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
