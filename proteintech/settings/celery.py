05fd3fa2922a1d1d882591cd00c4a431c3753336
Osigbemes <osinaikegbemileke@gmail.com>
2023-08-05 22:38:41 +0100

    celery tasks

--- proteintech/settings/celery.py
+++ proteintech/settings/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proteintech.settings.base')
 app = Celery('proteintech')
-app.config_from_object('django.conf:settings', 'proteintech.settings.base')
+app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs. 
 app.autodiscover_tasks()
@@ -10,6 +10,6 @@ app.autodiscover_tasks()
 # REDIS related settings 
 REDIS_HOST = 'localhost' 
 REDIS_PORT = '6379' 
-BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0' 
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT 
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600} 
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT 
