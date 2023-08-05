ecf721c1a84e62b9c91a1c4925ae5923221e2c20
Osigbemes <osinaikegbemileke@gmail.com>
2023-08-05 22:42:16 +0100

    celery tasks

--- proteintech/settings/celery.py
+++ proteintech/settings/celery.py
@@ -10,6 +10,11 @@ app.autodiscover_tasks()
 # REDIS related settings 
 REDIS_HOST = 'localhost' 
 REDIS_PORT = '6379' 
-BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT 
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0' 
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600} 
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT 
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+
+
+# Set broker_connection_retry and broker_connection_retry_on_startup
+CELERY_BROKER_CONNECTION_RETRY = True
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
